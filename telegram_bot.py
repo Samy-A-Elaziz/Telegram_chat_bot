d80b27955083d66bfc11f62d83565060d3ae8d77
Samy-A-Elaziz <122897943+Samy-A-Elaziz@users.noreply.github.com>
2023-08-20 15:16:40 +0300

    Add files via upload

---
+++ telegram_bot.py
@@ -0,0 +1,62 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final = "6649112697:AAF_MMhYxFS95v6sbZrtpFmeXydySROlsHY"
+BOT_USERNAME: Final = "@Resala_Training_Center_bot"
+
+#commands
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("hello in RTC.\nthe bot is under preparing")
+
+async def about_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("we are a nonprofit organization wish to spread knowledge all over EGYPT with free courses we pretend")
+
+#responses
+def handle_response(text: str) -> str:
+    processed: str = text.lower()
+
+    if 'hello' in processed:
+        return 'Hi'
+    else:
+        return "you can contact us on facebook page to know what you want\nhttps://www.facebook.com/RTCAlex"
+
+    return "invalid"
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f"user name: {update.message.from_user.username}user id {update.message.chat.id} in {message_type}: {text}")
+    
+    if message_type == "group":
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, "").strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+
+    print("Bot:", response)
+    await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"update {update} caused error {context.error}")
+
+
+if __name__ == "__main__":
+    print("starting RTC bot")
+    app = Application.builder().token(TOKEN).build()
+
+    #commands
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("about", about_command))
+
+    #messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    #errors
+    app.add_error_handler(error)
+
+    app.run_polling(poll_interval=3)
